121b8499401de0c315cc9ecfe855bad73f23a6e5
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-07-22 09:47:31 -0300

    descrição atividade

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Faça um script de javascript que gerencia (CRUD)
+// as atividades que o usuário fez nas férias,
+// o usuário diz o dia e
+// as atividades que realizou no dia.
+
 const prompt = require("prompt-sync")();
 const crud = require("./crud.js");
 
